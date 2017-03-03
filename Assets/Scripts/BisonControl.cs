bdc6010d7db8d97e77c2777832266b5f5121dd1d
juan callejas <TheShadyColombian@JuanBook-Air.local>
2017-03-03 11:33:18 -0500

    Added RequireComponent attribute to BisonControl.cs
    Added RequireComponent attribute to BisonControl.cs to make sure that
    the bison object it is applied to has a RigidBody2D component attached

--- Assets/Scripts/BisonControl.cs
+++ Assets/Scripts/BisonControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class BisonControl : MonoBehaviour
 {
     public float moveSpeed = 0.125f;
